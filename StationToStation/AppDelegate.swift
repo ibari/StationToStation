4a06978d8a4f815bd4fe2e9711de0c52e84fd718
Ian Bari <ian.bari@att.net>
2015-06-10 23:53:33 -0700

    Set navbar tint color

--- StationToStation/AppDelegate.swift
+++ StationToStation/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        setAppearance()
+        
         DataStoreClient.sharedInstance.onApplicationLaunch()
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "userDidLogout", name: userDidLogoutNotification, object: nil)
@@ -25,6 +27,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         
         return true
+    }
+    
+    func setAppearance() {
+        UINavigationBar.appearance().barTintColor = UIColor(red: 0.0/255.0, green: 142.0/255.0, blue: 212.0/255.0, alpha: 1.0)
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName : UIColor.whiteColor()]
+        
+        let navBarFont = UIFont(name: "HelveticaNeue", size:15.0)!
+        UIBarButtonItem.appearance().setTitleTextAttributes([NSFontAttributeName:navBarFont,NSForegroundColorAttributeName:UIColor.whiteColor()], forState: UIControlState.Normal)
+        
+        let tabBarFont = UIFont(name: "HelveticaNeue", size:12.0)!
+        UITabBarItem.appearance().setTitleTextAttributes([NSFontAttributeName:tabBarFont,NSForegroundColorAttributeName:UIColor.darkGrayColor()], forState: UIControlState.Normal)
     }
     
     func userDidLogout() {
